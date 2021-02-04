977e731536afd47446b69fadf39d0065b7b2cbdc
Max Joles <mpjoles@gmail.com>
2021-02-03 21:17:30 -0500

    Update meta-calc.js
    Stock returns finally working

--- meta-calc.js
+++ meta-calc.js
@@ -12,43 +12,42 @@
         event.preventDefault();
         var targetEbitda = $('input[name="targetEBITDA"]').val();
         var acquisitionMultiple = $('input[name="acquisitionMultiple"]').val();
-        var percentageCash = $('input[name="percentageCash"]').val();
-        var annualGrowth = $('input[name="annualGrowth"]').val();
-        var dividendYield = $('input[name="dividendYield"]').val();
-        var exitPremium = $('input[name="exitPremium"]').val();
-        var stockMarketReturns = $('input[name="stockMarketReturns"]').val();
-        var realEstateAppreciation = $('input[name="realEstateAppreciation"]').val();
-        var realEstateYield = $('input[name="realEstateYield"]').val();
-        var peHoldbackPercentage = $('input[name="peHoldbackPercentage"]').val();
+        var percentageCash = $('input[name="percentageCash"]').val() / 100;
+        var annualGrowth = $('input[name="annualGrowth"]').val() / 100;
+        var dividendYield = $('input[name="dividendYield"]').val() / 100;
+        var exitPremium = $('input[name="exitPremium"]').val() / 100;
+        var stockMarketReturns = $('input[name="stockMarketReturns"]').val() / 100;
+        var realEstateAppreciation = $('input[name="realEstateAppreciation"]').val() / 100;
+        var realEstateYield = $('input[name="realEstateYield"]').val() / 100;
+        var peHoldbackPercentage = $('input[name="peHoldbackPercentage"]').val() / 100;
         var peHoldbackYears = $('input[name="peHoldbackYears"]').val();
-        var conHoldbackPercetage = $('input[name="conHoldbackPercetage"]').val();
+        var conHoldbackPercetage = $('input[name="conHoldbackPercetage"]').val() / 100;
         var conHoldbackYears = $('input[name="conHoldbackYears"]').val();
 
         // Calculate fields based on inputs
-        var percentageRestrictedEquity = 100 - percentageCash;
-        $('input[name="percentageRestrictedEquity"]').val(percentageRestrictedEquity);
+        var percentageRestrictedEquity = 1 - percentageCash;
+        $('#percent-restricted-equity').text(percentageRestrictedEquity * 100);
 
-        var cashMultiple = acquisitionMultiple * (percentageCash / 100);
-        $('input[name="cashMultiple"]').val(cashMultiple);
+        var cashMultiple = acquisitionMultiple * percentageCash;
+        $('#cash-multiple').text((cashMultiple).toFixed(2));
 
-        var restrictedEquityMultiple = acquisitionMultiple * (percentageRestrictedEquity / 100);
-        $('input[name="restrictedEquityMultiple"]').val(restrictedEquityMultiple);
+        var restrictedEquityMultiple = acquisitionMultiple * percentageRestrictedEquity;
+        $('#restricted-equity-multiple').text((restrictedEquityMultiple).toFixed(2));
 
         var cash = targetEbitda * cashMultiple;
-        $('input[name="cash"]').val(cash);
+        $('#cash').text(formatter.format(cash));
 
         var restrictedEquity = targetEbitda * restrictedEquityMultiple;
-        $('input[name="restrictedEquity"]').val(restrictedEquity);
+        $('#restricted-equity').text(formatter.format(restrictedEquity));
 
 
 
         // 10 year restricted equity functions
-        var growthRate = 1 + (annualGrowth / 100);
-        var dividendRate = dividendYield / 100;
+        var growthRate = 1 + annualGrowth;
         for (let index = 0; index < 10; index++) {
             let headerLabel = index + 1;
             equityProjection = Math.round(restrictedEquity * (Math.pow(growthRate, index)));
-            annualDividend = Math.round(equityProjection * dividendRate);
+            annualDividend = Math.round(equityProjection * dividendYield);
             $("#restricted-equity-" + headerLabel).text(formatter.format(equityProjection));
             $("#annual-dividend-" + headerLabel).text(formatter.format(annualDividend));
 
@@ -60,7 +59,7 @@
             } else if (index == 9) {
                 let privateExit = equityProjection + annualDividend;
                 $("#private-exit-" + headerLabel).text(formatter.format(privateExit));
-                let publicExit = annualDividend + equityProjection * (1 + exitPremium / 100);
+                let publicExit = annualDividend + equityProjection * (1 + exitPremium);
                 $("#public-exit-" + headerLabel).text(formatter.format(publicExit));
 
             } else {
@@ -75,9 +74,11 @@
 
         // Cash + Investments
         var allCash = cash + restrictedEquity;
-        var stockReturns = 0;
+        let stockReturns = 0;
+        var marketRate = stockMarketReturns + 1;
         for (let index = 0; index < 10; index++) {
             let headerLabel = index + 1;
+            let pastMonth = index - 1;
             if (index == 0) {
                 $("#all-cash-" + headerLabel).text(formatter.format(allCash));
                 $("#cash-stock-" + headerLabel).text(formatter.format(allCash));
@@ -87,14 +88,13 @@
             else if (index == 9) {
                 $("#all-cash-" + headerLabel).text(formatter.format(0));
 
-
-
             } else {
-                stockReturns = (stockReturns + allCash) * stockMarketReturns/100;
+                stockReturns = (stockReturns + allCash) * stockMarketReturns;
+                stockInterest = Math.round(allCash * (Math.pow(marketRate, index))-(allCash * (Math.pow(marketRate, pastMonth))));
                 console.log(stockReturns);
-                let reReturns = allCash * realEstateYield/100;
+                let reReturns = allCash * realEstateYield;
                 $("#all-cash-" + headerLabel).text(formatter.format(0));
-                $("#cash-stock-" + headerLabel).text(formatter.format(stockReturns));
+                $("#cash-stock-" + headerLabel).text(formatter.format(stockInterest));
                 $("#cash-re-" + headerLabel).text(formatter.format(reReturns));
             }
         };
